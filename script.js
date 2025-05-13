252b00bdda7b440b24d6224b4b367449073f84e7
Ayon Kumar Saha <kumarayoninmail@gmail.com>
2025-05-13 19:46:04 +0530

    Initial commit for RailConnect landing page

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+// You can add interactivity here if needed in the future
+
+// Example: Smooth scroll for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
